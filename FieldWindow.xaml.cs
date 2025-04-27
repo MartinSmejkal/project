c0bcb104e501f3453eca5704fc2e184f9585a451
MS <257167@vutbr.cz>
2025-04-27 22:19:56 +0200

    Moved game field from main window to FieldWindow, created ExportGameDialog, created placeholder elements for getting parameters for field (used NumberUpdown elements from nugget -> UsedNuggets.txt)

---
+++ FieldWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace project
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class FieldWindow : Window
+    {
+        private Field field;
+        private HallOfFame hallOfFame;
+        public FieldWindow(string p1, string p2, byte fieldSize, byte winCondition, sbyte timerMax, HallOfFame hof)
+        {
+
+            InitializeComponent();
+            AdjustRowDefinitions(fieldSize);
+            field = new Field(p1, p2, fieldSize: fieldSize, winCondition: winCondition, timerMax: timerMax);
+            hallOfFame = hof;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button b = ((Button)sender);
+            int column = Grid.GetColumn(b);
+            int row = Grid.GetRow(b);
+            b.Content = field.PlayTurn(row, column);
+            if (field.CheckWin(row, column))
+            {
+                //temp
+                foreach (UIElement button in FieldGrid.Children)
+                {
+
+                    button.IsEnabled = false;
+                }
+                //TODO show pop-up window with the winner
+                Window main = this.Owner;
+                ExportGameDialog dialog = new ExportGameDialog();
+                bool? result = dialog.ShowDialog();
+                if (result == true)
+                {
+                    Game game = field.ExportGame();
+                    hallOfFame.AddGame(game);
+                }
+                this.Close();
+                main.Show();
+                return;
+            }
+            bool[,] playable = field.GetPlayableBoxes();
+            UIElementCollection buttons = FieldGrid.Children;
+            for (int rows = 0; rows < playable.GetLength(0); rows++)
+            {
+                for (int columns = 0; columns < playable.GetLength(1); columns++)
+                {
+                    foreach (UIElement button in buttons)
+                    {
+                        if (Grid.GetRow(button) == rows && Grid.GetColumn(button) == columns)
+                        {
+                            button.IsEnabled = playable[rows, columns];
+                        }
+                    }
+                }
+            }
+
+
+        }
+
+        private void AdjustRowDefinitions(int fieldSize)
+        {
+            FieldGrid.RowDefinitions.Clear();
+            FieldGrid.ColumnDefinitions.Clear();
+            for (int i = 0; i < fieldSize; i++)
+            {
+                RowDefinition rowDef = new RowDefinition();
+                rowDef.Height = new GridLength(1, GridUnitType.Star); //this sets the height of the row to *
+                FieldGrid.RowDefinitions.Add(rowDef);
+
+                ColumnDefinition colDef = new ColumnDefinition();
+                colDef.Width = new GridLength(1, GridUnitType.Star); //this sets the height of the row to *
+                FieldGrid.ColumnDefinitions.Add(colDef);
+            }
+
+            for (int rows = 0; rows < fieldSize; rows++)
+            {
+
+                for (int columns = 0; columns < fieldSize; columns++)
+                {
+                    Button b = new Button();
+                    b.Click += Button_Click;
+
+                    FieldGrid.Children.Add(b);
+                    Grid.SetColumn(b, columns);
+                    Grid.SetRow(b, rows);
+                }
+            }
+        }
+    }
+
+
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -9,6 +9,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
+
 namespace project
 {
     /// <summary>
@@ -16,78 +18,23 @@ namespace project
     /// </summary>
     public partial class MainWindow : Window
     {
-        private Field field = new Field("player1", "player2", fieldSize: 5, winCondition: 4, timerMax: 1);
+        private HallOfFame hallOfFame = new HallOfFame();
+
         public MainWindow()
         {
 
             InitializeComponent();
-            AdjustRowDefinitions(5);
+
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Button b = ((Button)sender);
-            int column = Grid.GetColumn(b);
-            int row = Grid.GetRow(b);
-            b.Content = field.PlayTurn(row, column);
-            if (field.CheckWin(row, column))
-            {
-                //temp
-                foreach (UIElement button in FieldGrid.Children)
-                {
-
-                    button.IsEnabled = false;
-                }
-                //TODO show pop-up window with the winner
-                return;
-            }
-            bool[,] playable = field.GetPlayableBoxes();
-            UIElementCollection buttons = FieldGrid.Children;
-            for (int rows = 0; rows < playable.GetLength(0); rows++)
-            {
-                for (int columns = 0; columns < playable.GetLength(1); columns++)
-                {
-                    foreach (UIElement button in buttons)
-                    {
-                        if (Grid.GetRow(button) == rows && Grid.GetColumn(button) == columns)
-                        {
-                            button.IsEnabled = playable[rows, columns];
-                        }
-                    }
-                }
-            }
-
+            FieldWindow window = new FieldWindow(player1.Text, player2.Text, (byte)size.Value, (byte)condition.Value, (sbyte)timer.Value, hallOfFame);
+            window.Owner = this;
+            this.Hide();
+            window.Show();
         }
 
-        private void AdjustRowDefinitions(int fieldSize)
-        {
-            FieldGrid.RowDefinitions.Clear();
-            FieldGrid.ColumnDefinitions.Clear();
-            for (int i = 0; i < fieldSize; i++)
-            {
-                RowDefinition rowDef = new RowDefinition();
-                rowDef.Height = new GridLength(1, GridUnitType.Star); //this sets the height of the row to *
-                FieldGrid.RowDefinitions.Add(rowDef);
-
-                ColumnDefinition colDef = new ColumnDefinition();
-                colDef.Width = new GridLength(1, GridUnitType.Star); //this sets the height of the row to *
-                FieldGrid.ColumnDefinitions.Add(colDef);
-            }
-
-            for (int rows = 0; rows < fieldSize; rows++)
-            {
-
-                for (int columns = 0; columns < fieldSize; columns++)
-                {
-                    Button b = new Button();
-                    b.Click += Button_Click;
-
-                    FieldGrid.Children.Add(b);
-                    Grid.SetColumn(b, columns);
-                    Grid.SetRow(b, rows);
-                }
-            }
-        }
     }
 
 
